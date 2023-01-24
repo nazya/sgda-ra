7aeea1e22a491bfe11ba5f8672e04bba2866d7ea
nazya <two_nazya@mail.ru>
2023-01-24 15:27:55 +0300

    utils

--- gamesopt/games/utils.py
+++ gamesopt/games/utils.py
@@ -9,53 +9,62 @@ def random_vector(dim: int) -> torch.Tensor:
     return x
 
 
-def generate_matrix(num_samples: int, dim: int,
-                    mu: float, ell: float) -> torch.Tensor:
-    A1 = torch.randn(dim, dim, requires_grad=False)
-    e, V = torch.linalg.eigh(A1.T@A1)
-    e -= e.min()
-    e /= e.max()
-    e *= ell - mu
-    e += mu
-    A1 = V @ torch.diag_embed(e) @ V.T
-    A2 = torch.randn(dim, dim, requires_grad=False)
-    A2 = A2.T@A2
-    A3 = torch.randn(dim, dim, requires_grad=False)
-    e, V = torch.linalg.eigh(A3.T@A3)
-    e -= e.min()
-    e /= e.max()
-    e *= ell - mu
-    e += mu
-    A3 = V @ torch.diag_embed(e) @ V.T
-    A12 = torch.cat((A1, A2), dim=0)
-    A23 = torch.cat((-A2.T, A3), dim=0)
-    A = torch.cat((A12, A23), dim=1)
-    return A
+# def generate_matrix(num_samples: int, dim: int,
+#                     mu: float, ell: float) -> torch.Tensor:
+#     A1 = torch.randn(dim, dim, requires_grad=False)
+#     e, V = torch.linalg.eigh(A1.T@A1)
+#     e -= e.min()
+#     e /= e.max()
+#     e *= ell - mu
+#     e += mu
+#     A1 = V @ torch.diag_embed(e) @ V.T
+#     A2 = torch.randn(dim, dim, requires_grad=False)
+#     A2 = A2.T@A2
+#     A3 = torch.randn(dim, dim, requires_grad=False)
+#     e, V = torch.linalg.eigh(A3.T@A3)
+#     e -= e.min()
+#     e /= e.max()
+#     e *= ell - mu
+#     e += mu
+#     A3 = V @ torch.diag_embed(e) @ V.T
+#     A12 = torch.cat((A1, A2), dim=0)
+#     A23 = torch.cat((-A2.T, A3), dim=0)
+#     A = torch.cat((A12, A23), dim=1)
+#     return A
 
 
-def create_matrix(config):
-    A1 = torch.randn(config.num_samples, config.dim, config.dim,
+def create_matrix(dim, num_samples, mu, ell, sigma, with_bias):
+    A1 = torch.randn(num_samples, dim, dim,
                      requires_grad=False)
     e, V = torch.linalg.eigh(A1@A1.transpose(1, 2))
     e -= torch.min(e, 1)[0][:, None]
     e /= torch.max(e, 1)[0][:, None]
-    e *= config.ell - config.mu
-    e += config.mu
+    e *= ell - mu
+    e += mu
     A1 = V @ torch.diag_embed(e) @ V.transpose(1, 2)
-    A2 = torch.randn(config.num_samples, config.dim, config.dim,
+    A2 = torch.randn(num_samples, dim, dim,
                      requires_grad=False)
     A2 = A2@A2.transpose(1, 2)
-    A3 = torch.randn(config.num_samples, config.dim, config.dim,
+    A3 = torch.randn(num_samples, dim, dim,
                      requires_grad=False)
     e, V = torch.linalg.eigh(A3@A3.transpose(1, 2))
     e -= torch.min(e, 1)[0][:, None]
     e /= torch.max(e, 1)[0][:, None]
-    e *= config.ell - config.mu
-    e += config.mu
+    e *= ell - mu
+    e += mu
     A3 = V @ torch.diag_embed(e) @ V.transpose(1, 2)
     A12 = torch.cat((A1, A2), dim=1)
     A23 = torch.cat((-A2.transpose(1, 2), A3), dim=1)
     A = torch.cat((A12, A23), dim=2)
+    bias = torch.zeros(num_samples, 2*dim)
+
+    mean_A = A.mean(dim=0)
+    A = (1-sigma)*mean_A + sigma*A
+
+    if with_bias:
+        bias = 10*bias.normal_() / math.sqrt(2*dim)
+        mean_b = bias.mean(dim=0)
+        bias = (1-sigma)*mean_b + sigma*bias
     # s, _ = torch.linalg.eigh(A[0])
     # print('Matrix generated', s.min(), s.max())
-    return A
+    return A, bias
